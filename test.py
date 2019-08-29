c96e15ff46d33b3dc71434cb8bf0b44c3246858f
Jorge Garcia <43189738+Jagarcia48@users.noreply.github.com>
2019-08-29 01:11:43 -0600

    Add files via upload
    finished sorting working on reading from user and writting

---
+++ test.py
@@ -0,0 +1,58 @@
+#Jorge Garcia
+#CS4310
+#August 28,2019
+#Update: finished sorting working on reading file and writting
+
+def test(input):
+    myfile = open(input,'r')
+    lines = myfile.readlines()
+
+    # creates empty lists to store words and keep track of # of words used
+    wordlist = []
+    counter = []
+
+    for line in lines:
+        # get all the words in the current line
+        words = line.split()
+        # iterates through each individual word 
+        for word in words:
+            # removes punctuations
+            # makes the words case insensitive
+            word = word.strip(".,!?:;'\"") 
+            word = word.lower()
+
+            if word not in wordlist:
+                wordlist.append(word)
+
+            # adds new word to counter
+            counter.append(word)
+
+    wordlist.sort()
+
+    for word in wordlist:
+        print(word, counter.count(word))
+test(input())
+
+
+
+
+#words = sorted(open("declaration.txt").read().split(),key = str.casefold)
+#print(words)
+
+
+
+#f = open("declaration.txt", "r")
+
+#words = sorted(f.read().split(), key = str.casefold)
+
+#for  in words:
+#    print(word)
+
+
+#words = sorted(open("declaration.txt").read().split())
+#print(words)
+#
+#f = open("speech.txt", "r")
+#print(f.read())
+#
+
